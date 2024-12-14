fec3f5d5131374fa8ae0e0ab1444166abbfc743e
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2024-12-14 11:05:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-app.use(express.json());
 require('dotenv').config();
+app.use(express.json());
+
 
 app.use(cors({ // origin 'http://localhost:3000' has been blocked by
   "origin": "*",
