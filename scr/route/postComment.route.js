9de73bc27862b6ed955b06f002f42338246d4cbe
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:04:10 +0700

    Update postComment.route.js

--- scr/route/postComment.route.js
+++ scr/route/postComment.route.js
@@ -1,8 +1,10 @@
 const post_comment_controller = require("../controller/postComment.controller")
+const { CheckToken } = require("../controller/user.controller");
 const postComment = (app) =>{
     app.get("/api/postComment/getList",post_comment_controller.getList);
-    app.post("/api/postComment/create",post_comment_controller.create);
-    app.put("/api/postComment/update",post_comment_controller.update);
-    app.delete("/api/postComment/delete",post_comment_controller.remove); 
+    
+    app.post("/api/postComment/create",CheckToken(),post_comment_controller.create);
+    app.put("/api/postComment/update",CheckToken(),post_comment_controller.update);
+    app.delete("/api/postComment/delete",CheckToken(),post_comment_controller.remove); 
 }
 module.exports = postComment;
