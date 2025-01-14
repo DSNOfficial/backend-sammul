ac94530769f278a53d2549d9db52fc90a502f50b
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:55:48 +0700

    Update valueH.route.js

--- scr/route/valueH.route.js
+++ scr/route/valueH.route.js
@@ -1,15 +1,15 @@
 
 const valueH_controller = require("../controller/valueH.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const valueH = (app) =>{
  
     app.get("/api/values/getList",valueH_controller.getList);
     app.post("/api/values/getone",valueH_controller.getOne);
    // app.use(CheckToken());
-    app.post("/api/values/create",upload.single("image"),valueH_controller.create);
-    app.put("/api/values/update",upload.single("image"),valueH_controller.update);
-    app.delete("/api/values/delete",valueH_controller.remove);    
+    app.post("/api/values/create",CheckToken(),upload.single("image"),valueH_controller.create);
+    app.put("/api/values/update",CheckToken(),upload.single("image"),valueH_controller.update);
+    app.delete("/api/values/delete",CheckToken(),valueH_controller.remove);    
 }
 module.exports = valueH;
 
