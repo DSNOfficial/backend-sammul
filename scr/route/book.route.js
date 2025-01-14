8a1d49de0f2de7e1647d2b637111b6d08db20fa6
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:10:06 +0700

    Update book.route.js

--- scr/route/book.route.js
+++ scr/route/book.route.js
@@ -1,5 +1,5 @@
 const book_controller = require("../controller/book.controller");
-// const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const { upload_file } = require("../config/helper");
 
 const book = (app) => {
@@ -7,9 +7,10 @@ const book = (app) => {
 
     app.get("/api/book/getList", book_controller.getList);
     app.get("/api/book/:id", book_controller.getOne); // Use GET for fetching one book by ID
-    app.post("/api/book/create", upload_file.single('file'), book_controller.create);
-    app.put("/api/book/update/", upload_file.single('File'), book_controller.update);
-    app.delete("/api/book/delete", book_controller.remove);
+    
+    app.post("/api/book/create",CheckToken(), upload_file.single('file'), book_controller.create);
+    app.put("/api/book/update/",CheckToken(), upload_file.single('File'), book_controller.update);
+    app.delete("/api/book/delete",CheckToken(), book_controller.remove);
 
 
 }
