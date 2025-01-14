3777e39ebeabfe3f26d59bb78bdd5e909aa67913
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:08:11 +0700

    Update leader.route.js

--- scr/route/leader.route.js
+++ scr/route/leader.route.js
@@ -1,13 +1,15 @@
 
 const leader_controller = require("../controller/leader.controller");
 const {upload} = require("../config/helper");
+const { CheckToken } = require("../controller/user.controller");
 const leader = (app) =>{
 
     app.get("/api/leader/getList",leader_controller.getList);
     app.get("/api/leader/getone/:id",leader_controller.getOne);
-    app.post("/api/leader/create",upload.single("image"),leader_controller.create);
-    app.put("/api/leader/update",upload.single("image"),leader_controller.update);
-    app.delete("/api/leader/delete",leader_controller.remove);    
+    
+    app.post("/api/leader/create",CheckToken(),upload.single("image"),leader_controller.create);
+    app.put("/api/leader/update",CheckToken(),upload.single("image"),leader_controller.update);
+    app.delete("/api/leader/delete",CheckToken(),leader_controller.remove);    
 }
 module.exports = leader;
 
