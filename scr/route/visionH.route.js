58a9299bbb3aed7d337e2df4fd12ba2e13bceb9d
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:56:19 +0700

    Update visionH.route.js

--- scr/route/visionH.route.js
+++ scr/route/visionH.route.js
@@ -1,15 +1,15 @@
 
 const visionH_controller = require("../controller/visionH.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const visionH = (app) =>{
  
     app.get("/api/vision/getList",visionH_controller.getList);
     app.post("/api/vision/getone",visionH_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/vision/create",upload.single("image"),visionH_controller.create);
-    app.put("/api/vision/update",upload.single("image"),visionH_controller.update);
-    app.delete("/api/vision/delete",visionH_controller.remove);    
+ 
+    app.post("/api/vision/create",CheckToken(),upload.single("image"),visionH_controller.create);
+    app.put("/api/vision/update",CheckToken(),upload.single("image"),visionH_controller.update);
+    app.delete("/api/vision/delete",CheckToken(),visionH_controller.remove);    
 }
 module.exports = visionH;
 
