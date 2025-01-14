0c88634ec8cbd8826c856823f6dad455a93bf8bf
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:08:31 +0700

    Update history.route.js

--- scr/route/history.route.js
+++ scr/route/history.route.js
@@ -1,13 +1,15 @@
 
 const history_controller = require("../controller/history.controller");
 const {upload} = require("../config/helper");
+const { CheckToken } = require("../controller/user.controller");
 const history = (app) =>{
 
     app.get("/api/history/getList",history_controller.getList);
     app.get("/api/history/getone/:id",history_controller.getOne);
-    app.post("/api/history/create",upload.single("image"),history_controller.create);
-    app.put("/api/history/update",upload.single("image"),history_controller.update);
-    app.delete("/api/history/delete",history_controller.remove);    
+
+    app.post("/api/history/create",CheckToken(),upload.single("image"),history_controller.create);
+    app.put("/api/history/update",CheckToken(),upload.single("image"),history_controller.update);
+    app.delete("/api/history/delete",CheckToken(),history_controller.remove);    
 }
 module.exports = history;
 
