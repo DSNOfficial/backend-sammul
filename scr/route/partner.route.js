1ad4f5cb258605c5715cf19b380166fcd81d7806
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:05:47 +0700

    Update partner.route.js

--- scr/route/partner.route.js
+++ scr/route/partner.route.js
@@ -1,15 +1,15 @@
 
 const partner_controller = require("../controller/partner.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const partner = (app) =>{
  
     app.get("/api/partner/getList",partner_controller.getList);
     app.post("/api/partner/getone",partner_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/partner/create",upload.single("image"),partner_controller.create);
-    app.put("/api/partner/update",upload.single("image"),partner_controller.update);
-    app.delete("/api/partner/delete",partner_controller.remove);    
+   
+    app.post("/api/partner/create",CheckToken(),upload.single("image"),partner_controller.create);
+    app.put("/api/partner/update",CheckToken(),upload.single("image"),partner_controller.update);
+    app.delete("/api/partner/delete",CheckToken(),partner_controller.remove);    
 }
 module.exports = partner;
 
