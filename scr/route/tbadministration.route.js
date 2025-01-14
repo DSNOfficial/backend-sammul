7ead722af0ebfc6caa91fb35c27bbbaedbcd02e6
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:58:48 +0700

    Update tbadministration.route.js

--- scr/route/tbadministration.route.js
+++ scr/route/tbadministration.route.js
@@ -1,15 +1,15 @@
 
 const administration_controller = require("../controller/tbadministration.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const administration = (app) =>{
  
     app.get("/api/administration/getList",administration_controller.getList);
     app.post("/api/administration/getone",administration_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/administration/create",upload.single("image"),administration_controller.create);
-    app.put("/api/administration/update",upload.single("image"),administration_controller.update);
-    app.delete("/api/administration/delete",administration_controller.remove);    
+   
+    app.post("/api/administration/create",CheckToken(),upload.single("image"),administration_controller.create);
+    app.put("/api/administration/update",CheckToken(),upload.single("image"),administration_controller.update);
+    app.delete("/api/administration/delete",CheckToken(),administration_controller.remove);    
 }
 module.exports = administration;
 
