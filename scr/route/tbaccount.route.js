856c317015262bcf609aa9724fd48257369cf2a0
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:00:22 +0700

    Update tbaccount.route.js

--- scr/route/tbaccount.route.js
+++ scr/route/tbaccount.route.js
@@ -1,15 +1,15 @@
 
 const account_controller = require("../controller/tbaccount.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const account = (app) =>{
  
     app.get("/api/account/getList",account_controller.getList);
     app.post("/api/account/getone",account_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/account/create",upload.single("image"),account_controller.create);
-    app.put("/api/account/update",upload.single("image"),account_controller.update);
-    app.delete("/api/account/delete",account_controller.remove);    
+   
+    app.post("/api/account/create",CheckToken(),upload.single("image"),account_controller.create);
+    app.put("/api/account/update",CheckToken(),upload.single("image"),account_controller.update);
+    app.delete("/api/account/delete",CheckToken(),account_controller.remove);    
 }
 module.exports = account;
 
