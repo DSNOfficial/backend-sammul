338d9ee65de235facba098aa5cf6e841a9ce8761
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:57:42 +0700

    Update tbpost.route.js

--- scr/route/tbpost.route.js
+++ scr/route/tbpost.route.js
@@ -1,14 +1,14 @@
 
 const tbpost_controller = require("../controller/tbpost.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const tbpost = (app) =>{
 
     app.get("/api/tbpost/getList",tbpost_controller.getList);
-    //app.use(CheckToken());
-    app.post("/api/tbpost/create",upload.single("image"),tbpost_controller.create);
-    app.put("/api/tbpost/update",upload.single("image"),tbpost_controller.update);
-    app.delete("/api/tbpost/delete",tbpost_controller.remove);    
+   
+    app.post("/api/tbpost/create",CheckToken(),upload.single("image"),tbpost_controller.create);
+    app.put("/api/tbpost/update",CheckToken(),upload.single("image"),tbpost_controller.update);
+    app.delete("/api/tbpost/delete",CheckToken(),tbpost_controller.remove);    
 }
 module.exports = tbpost;
 
