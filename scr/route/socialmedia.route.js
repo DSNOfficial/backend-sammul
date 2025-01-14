edd30325b0b7adec1958238996f992ea0ecc62de
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:00:52 +0700

    Update socialmedia.route.js

--- scr/route/socialmedia.route.js
+++ scr/route/socialmedia.route.js
@@ -1,11 +1,11 @@
 
 const socialmedia_controller = require("../controller/category.controller");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const socialmedia  = (app) =>{
     app.get("/api/socialmedia/getList",socialmedia_controller.getList);
-   // app.use(CheckToken());
-    app.post("/api/socialmedia/create",socialmedia_controller.create);
-    app.put("/api/socialmedia/update",socialmedia_controller.update);
-    app.delete("/api/socialmedia/delete",socialmedia_controller.remove); 
+  
+    app.post("/api/socialmedia/create",CheckToken(),socialmedia_controller.create);
+    app.put("/api/socialmedia/update",CheckToken(),socialmedia_controller.update);
+    app.delete("/api/socialmedia/delete",CheckToken(),socialmedia_controller.remove); 
 }
 module.exports = socialmedia;
