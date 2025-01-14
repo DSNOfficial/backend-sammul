1d484e7a36a0dc25e5b70b79dfc320418ed8d011
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:03:49 +0700

    Update postMeta.route.js

--- scr/route/postMeta.route.js
+++ scr/route/postMeta.route.js
@@ -1,8 +1,10 @@
 const postMeta_controller = require("../controller/postMeta.controller")
+const { CheckToken } = require("../controller/user.controller");
 const postMeta = (app) =>{ 
     app.get("/api/postMeta/getList",postMeta_controller.getList);
-    app.post("/api/postMeta/create",postMeta_controller.create);
-    app.put("/api/postMeta/update",postMeta_controller.update);
-    app.delete("/api/postMeta/delete",postMeta_controller.remove); 
+    
+    app.post("/api/postMeta/create",CheckToken(),postMeta_controller.create);
+    app.put("/api/postMeta/update",CheckToken(),postMeta_controller.update);
+    app.delete("/api/postMeta/delete",CheckToken(),postMeta_controller.remove); 
 }
 module.exports = postMeta;
