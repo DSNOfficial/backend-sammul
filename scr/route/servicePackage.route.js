346068308b1033e96b5882711877d9c1b9eeb575
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:02:12 +0700

    Update servicePackage.route.js

--- scr/route/servicePackage.route.js
+++ scr/route/servicePackage.route.js
@@ -1,14 +1,14 @@
 
 const servicePackage_controller = require("../controller/servicePackage.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const servicePackage = (app) =>{
-    // app.use(CheckToken());
+
     app.get("/api/servicePackage/getlist",servicePackage_controller.getList);
     app.post("/api/servicePackage/getone",servicePackage_controller.getOne);
-    //app.use(CheckToken());
-    app.post("/api/servicePackage/create",upload.single("image"),servicePackage_controller.create);
-    app.put("/api/servicePackage/update",upload.single("image"), servicePackage_controller.update);
-    app.delete("/api/servicePackage/delete",servicePackage_controller.remove); 
+    
+    app.post("/api/servicePackage/create",CheckToken(),upload.single("image"),servicePackage_controller.create);
+    app.put("/api/servicePackage/update",CheckToken(),upload.single("image"), servicePackage_controller.update);
+    app.delete("/api/servicePackage/delete",CheckToken(),servicePackage_controller.remove); 
 }
 module.exports = servicePackage;
