2972944be624a4eb4353bd64b8e32a4d8294767d
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:56:50 +0700

    Update training.route.js

--- scr/route/training.route.js
+++ scr/route/training.route.js
@@ -1,15 +1,15 @@
 
 const training_controller = require("../controller/training.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const training = (app) =>{
  
     app.get("/api/training/getList",training_controller.getList);
     app.post("/api/training/getone",training_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/training/create",upload.single("image"),training_controller.create);
-    app.put("/api/training/update",upload.single("image"),training_controller.update);
-    app.delete("/api/training/delete",training_controller.remove);    
+  
+    app.post("/api/training/create",CheckToken(),upload.single("image"),training_controller.create);
+    app.put("/api/training/update",CheckToken(),upload.single("image"),training_controller.update);
+    app.delete("/api/training/delete",CheckToken(),training_controller.remove);    
 }
 module.exports = training;
 
