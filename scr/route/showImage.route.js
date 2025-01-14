06c1197cc233e3625fc0db881ec9a068a70cd135
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:01:44 +0700

    Update showImage.route.js

--- scr/route/showImage.route.js
+++ scr/route/showImage.route.js
@@ -1,14 +1,14 @@
 
 const showImage_controller = require("../controller/showImage.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const showImage = (app) =>{
   
     app.get("/api/showImage/getList",showImage_controller.getList);
-    //app.use(CheckToken());
-    app.post("/api/showImage/create",upload.single("image"),showImage_controller.create);
-    app.put("/api/showImage/update",upload.single("image"),showImage_controller.update);
-    app.delete("/api/showImage/delete",showImage_controller.remove);    
+    
+    app.post("/api/showImage/create",CheckToken(),upload.single("image"),showImage_controller.create);
+    app.put("/api/showImage/update",CheckToken(),upload.single("image"),showImage_controller.update);
+    app.delete("/api/showImage/delete",CheckToken(),showImage_controller.remove);    
 }
 module.exports = showImage;
 
