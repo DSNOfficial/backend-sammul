b8bfae9e6f23148604454b1e438f82d7c2d29199
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:01:23 +0700

    Update slideShow.route.js

--- scr/route/slideShow.route.js
+++ scr/route/slideShow.route.js
@@ -1,13 +1,13 @@
 
 const slideShow_controller = require("../controller/slideShow.controller");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const {upload_multi} = require("../config/helper");
 
 const slideShow = (app) =>{
     app.get("/api/slideShow/getList",slideShow_controller.getList);
-    //app.use(CheckToken());
-    app.post("/api/slideShow/create",upload_multi.array("images",10),slideShow_controller.create);
-    app.put("/api/slideShow/update", upload_multi.single("images",10), slideShow_controller.update);
-    app.delete("/api/slideShow/delete",slideShow_controller.remove); 
+   
+    app.post("/api/slideShow/create",CheckToken(),upload_multi.array("images",10),slideShow_controller.create);
+    app.put("/api/slideShow/update", CheckToken(),upload_multi.single("images",10), slideShow_controller.update);
+    app.delete("/api/slideShow/delete",CheckToken(),slideShow_controller.remove); 
 }
 module.exports = slideShow;
