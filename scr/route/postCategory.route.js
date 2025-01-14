97b18d2c3413912858ffe33d60ba041af9b46268
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:04:32 +0700

    Update postCategory.route.js

--- scr/route/postCategory.route.js
+++ scr/route/postCategory.route.js
@@ -1,9 +1,11 @@
 
 const postCategory_controller = require("../controller/postCategory.controller")
+const { CheckToken } = require("../controller/user.controller");
 const postCategory = (app) =>{
     app.get("/api/postCategory/getList",postCategory_controller.getList);
-    app.post("/api/postCategory/create",postCategory_controller.create);
-    app.put("/api/postCategory/update",postCategory_controller.update);
-    app.delete("/api/postCategory/delete",postCategory_controller.remove); 
+    
+    app.post("/api/postCategory/create",CheckToken(),postCategory_controller.create);
+    app.put("/api/postCategory/update",CheckToken(),postCategory_controller.update);
+    app.delete("/api/postCategory/delete",CheckToken(),postCategory_controller.remove); 
 }
 module.exports = postCategory;
