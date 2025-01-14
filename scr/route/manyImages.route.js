791ed2d4f4ef13e34d7dda854663c1fd3f63af90
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:07:33 +0700

    Update manyImages.route.js

--- scr/route/manyImages.route.js
+++ scr/route/manyImages.route.js
@@ -1,15 +1,16 @@
 
 const manyImages_controller = require("../controller/manyImages.controller");
-
+const { CheckToken } = require("../controller/user.controller");
 const {upload} = require("../config/helper");
 
 const manyImages = (app) =>{
 
     app.get("/api/manyImages/getList",manyImages_controller.getlist);
     app.get("/api/manyImages/getOne",manyImages_controller.getone);
-    app.post("/api/manyImages/create",upload.array("images",10),manyImages_controller.create);
-    app.put("/api/manyImages/update",upload.single('image'),manyImages_controller.update);
-    app.delete("/api/manyImages/delete",manyImages_controller.remove); 
+
+    app.post("/api/manyImages/create",CheckToken(),upload.array("images",10),manyImages_controller.create);
+    app.put("/api/manyImages/update",CheckToken(),upload.single('image'),manyImages_controller.update);
+    app.delete("/api/manyImages/delete",CheckToken(),manyImages_controller.remove); 
 
 }
 module.exports = manyImages;
