bf378e36bb3930e30746a079e0af7ac4b9f159fd
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 19:03:24 +0700

    Update postnew.route.js

--- scr/route/postnew.route.js
+++ scr/route/postnew.route.js
@@ -1,12 +1,13 @@
 const postnew_controller = require('../controller/postnew.controller');
 const { upload } = require('../config/helper');
+const { CheckToken } = require("../controller/user.controller");
 
 const postnew = (app) => {
     app.get('/api/postnew/getlist', postnew_controller.getList);
     // app.get('/api/postnew/getOne', postnew_controller.getOne);
-    app.post('/api/postnew/create', upload.array('images', 10), postnew_controller.create);
-    app.put('/api/postnew/update', upload.array('images', 10), postnew_controller.update);
-    app.delete('/api/postnew/delete', postnew_controller.remove);
+    app.post('/api/postnew/create',CheckToken(),upload.array('images', 10), postnew_controller.create);
+    app.put('/api/postnew/update',CheckToken(),upload.array('images', 10), postnew_controller.update);
+    app.delete('/api/postnew/delete',CheckToken(),postnew_controller.remove);
 };
 
 module.exports = postnew;
