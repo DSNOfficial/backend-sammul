805441f87dd1d3600fa5be735e9a9d8d46901510
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:58:13 +0700

    Update tbmarquee.route.js

--- scr/route/tbmarquee.route.js
+++ scr/route/tbmarquee.route.js
@@ -1,15 +1,15 @@
 
 const tbmarquee_controller = require("../controller/tbmarquee.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const tbmarquee = (app) =>{
  
     app.get("/api/tbmarquee/getList",tbmarquee_controller.getList);
     app.post("/api/tbmarquee/getone",tbmarquee_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/tbmarquee/create",upload.single("image"),tbmarquee_controller.create);
-    app.put("/api/tbmarquee/update",upload.single("image"),tbmarquee_controller.update);
-    app.delete("/api/tbmarquee/delete",tbmarquee_controller.remove);    
+   
+    app.post("/api/tbmarquee/create",CheckToken(),upload.single("image"),tbmarquee_controller.create);
+    app.put("/api/tbmarquee/update",CheckToken(),upload.single("image"),tbmarquee_controller.update);
+    app.delete("/api/tbmarquee/delete",CheckToken(),tbmarquee_controller.remove);    
 }
 module.exports = tbmarquee;
 
