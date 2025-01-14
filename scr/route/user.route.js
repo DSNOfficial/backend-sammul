2568670265207b881e82c34593ea4028d50be3c7
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:55:21 +0700

    Update user.route.js

--- scr/route/user.route.js
+++ scr/route/user.route.js
@@ -1,20 +1,21 @@
 
 const user_controller = require("../controller/user.controller");
-// const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 
 const user = (app) => {
     // Public routes
     
-    app.get("/api/user/getList", user_controller.getList);
+   
     app.post("/api/user/login", user_controller.login);
-    app.post("/api/refresh_token", user_controller.refresh_token);
-    //  app.use(CheckToken());
-    app.post("/api/user/create", user_controller.create);
-    app.put("/api/user/update", user_controller.update);
-    app.post("/api/user/setPassword", user_controller.setPassword);
-    app.delete("/api/user/delete", user_controller.remove);
-      // Protected routes
-    // app.use(CheckToken()); // Apply middleware to all routes defined after this line
+    app.post("/api/refresh_token",CheckToken(), user_controller.refresh_token);
+
+
+    app.get("/api/user/getList",CheckToken(), user_controller.getList);
+    app.post("/api/user/create",CheckToken(), user_controller.create);
+    app.put("/api/user/update", CheckToken(),user_controller.update);
+    app.post("/api/user/setPassword", CheckToken(),user_controller.setPassword);
+    app.delete("/api/user/delete",CheckToken(), user_controller.remove);
+  
 };
 
 module.exports = user;
