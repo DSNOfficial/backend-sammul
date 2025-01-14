f339b1ac93120488e0767516b457a466a052de84
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-01-14 18:57:17 +0700

    Update tbtechnical.route.js

--- scr/route/tbtechnical.route.js
+++ scr/route/tbtechnical.route.js
@@ -1,15 +1,15 @@
 
 const technical_controller = require("../controller/tbtechnical.controller");
 const {upload} = require("../config/helper");
-//const { CheckToken } = require("../controller/user.controller");
+const { CheckToken } = require("../controller/user.controller");
 const technical = (app) =>{
  
     app.get("/api/technical/getList",technical_controller.getList);
     app.post("/api/technical/getone",technical_controller.getOne);
-   // app.use(CheckToken());
-    app.post("/api/technical/create",upload.single("image"),technical_controller.create);
-    app.put("/api/technical/update",upload.single("image"),technical_controller.update);
-    app.delete("/api/technical/delete",technical_controller.remove);    
+   
+    app.post("/api/technical/create",CheckToken(),upload.single("image"),technical_controller.create);
+    app.put("/api/technical/update",CheckToken(),upload.single("image"),technical_controller.update);
+    app.delete("/api/technical/delete",CheckToken(),technical_controller.remove);    
 }
 module.exports = technical;
 
