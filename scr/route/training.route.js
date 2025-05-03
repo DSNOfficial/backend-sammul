a08766705b9cf13447951951159120a7928324ee
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-05-03 15:47:07 +0700

    Update training.route.js

--- scr/route/training.route.js
+++ scr/route/training.route.js
@@ -1,16 +1,30 @@
-
 const training_controller = require("../controller/training.controller");
-const {upload} = require("../config/helper");
+const { upload } = require("../config/helper");
 const { CheckToken } = require("../controller/user.controller");
-const training = (app) =>{
- 
-    app.get("/api/training/getList",training_controller.getList);
-    app.post("/api/training/getone",training_controller.getOne);
-  
-    app.post("/api/training/create",CheckToken(),upload.single("image"),training_controller.create);
-    app.put("/api/training/update",CheckToken(),upload.single("image"),training_controller.update);
-    app.delete("/api/training/delete",CheckToken(),training_controller.remove);    
-}
+
+const training = (app) => {
+    app.get("/api/training/getList", training_controller.getList);
+    app.post("/api/training/getone", training_controller.getOne);
+    app.get("/api/tbtraining_image/:training_id",training_controller.TrainImage);
+    app.post(
+        "/api/training/create",
+        CheckToken(),
+        upload.fields([
+            { name: 'upload_image', maxCount: 1 },
+            { name: 'upload_image_optional', maxCount: 15 }
+        ]),
+        training_controller.create
+    );
+    app.put(
+        "/api/training/update",
+        CheckToken(),
+        upload.fields([
+            { name: 'upload_image', maxCount: 1 },
+            { name: 'upload_image_optional', maxCount: 15 }
+        ]),
+        training_controller.update
+    );
+    app.delete("/api/training/delete", CheckToken(), training_controller.remove);
+};
+
 module.exports = training;
-
-
