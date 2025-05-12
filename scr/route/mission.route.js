539c8de1999203fe662716d115ee9842102860e2
DSN Official <78654701+DSNOfficial@users.noreply.github.com>
2025-05-12 11:58:53 +0700

    Update mission.route.js

--- scr/route/mission.route.js
+++ scr/route/mission.route.js
@@ -6,8 +6,8 @@ const mission = (app) =>{
     app.get("/api/mission/getList",mission_controller.getList);
     app.post("/api/mission/getone",mission_controller.getOne);
  
-    app.post("/api/mission/create",CheckToken(),upload.single("image"),mission_controller.create);
-    app.put("/api/mission/update",CheckToken(),upload.single("image"),mission_controller.update);
+    app.post("/api/mission/create",CheckToken(),upload.single("upload_image"),mission_controller.create);
+    app.put("/api/mission/update",CheckToken(),upload.single("upload_image"),mission_controller.update);
     app.delete("/api/mission/delete",CheckToken(),mission_controller.remove);    
 }
 module.exports = mission;
